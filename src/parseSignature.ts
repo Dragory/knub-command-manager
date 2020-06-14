06871ee7c79d6f5242da3f4c31f2a7a44827f01d
Dragory <2606411+Dragory@users.noreply.github.com>
2020-06-14 17:03:47 +0300

    Remove deprecated code from parseSignature.ts

--- src/parseSignature.ts
+++ src/parseSignature.ts
@@ -1,19 +1,6 @@
 import { IParameter, TOption, TSignature, TTypeConverterFn } from "./types";
 import { defaultTypeConverters } from "./defaultTypes";
 
-const paramDefinitionSimpleRegex = /[<\[].*?[>\]]/g;
-
-const paramDefinitionRegex = new RegExp(
-  "[<\\[]" +
-  "([a-z0-9]+?)" + // (1) Argument name
-  "(?:\\:([a-z]+?))?" + // (2) Argument type
-  "(?:=(.+?))?" + // (3) Default value
-  "(\\.\\.\\.)?" + // (4) "..." to mark argument as a rest argument
-  "(\\$)?" + // (5) "$" to mark the argument as a "catch-all" for the rest of the arguments (will be returned as the full string, unlike "...")
-    "[>\\]]",
-  "i",
-);
-
 export function parseSignature<TContext = any>(
   str: string,
   types: Record<string, TTypeConverterFn<TContext>> = defaultTypeConverters,
@@ -144,43 +131,4 @@ export function parseSignature<TContext = any>(
   }
 
   return result;
-}
-
-export function _parseParameters<TContext = any>(
-  str: string,
-  types: Record<string, TTypeConverterFn<TContext>> = defaultTypeConverters,
-  defaultType = "string",
-): Record<string, IParameter<TContext>> {
-  const parameterDefinitions = str.match(paramDefinitionSimpleRegex) || [];
-
-  return parameterDefinitions.reduce((parameters, parameterDefinition, i) => {
-    const details = parameterDefinition.match(paramDefinitionRegex);
-    if (!details) {
-      throw new Error(`Invalid parameter definition: ${parameterDefinition}`);
-    }
-
-    let defaultValue: any = details[3];
-    const isRest = details[4] === "...";
-    const isOptional = parameterDefinition[0] === "[" || defaultValue != null;
-    const isCatchAll = details[5] === "$";
-
-    if (isRest) {
-      defaultValue = [];
-    }
-
-    const typeName = details[2] || defaultType;
-    if (types[typeName] == null) {
-      throw new Error(`Unknown parameter type: ${typeName}`);
-    }
-
-    parameters[details[1]] = {
-      type: types[typeName],
-      required: !isOptional,
-      def: defaultValue,
-      rest: isRest,
-      catchAll: isCatchAll,
-    };
-
-    return parameters;
-  }, {});
 }
